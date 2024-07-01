9f333008c10aa9609312f7bac702a465f494ee06
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-07-01 22:03:11 +0530

    add: Error component

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,10 +1,19 @@
-const Error = () => {
+import './Error.css';
+import PropTypes from 'prop-types';
+
+const Error = ({ status = 404, message = 'Page not found' }) => {
   return (
-    <>
-      <h1>404</h1>
-      <h2>Page not found!</h2>
-    </>
+    <div className='error'>
+      <h1 className='head'>{status}</h1>
+      <h2 className='message'>{message}</h2>
+      <img className='loading' src='/icon.svg' alt='logo' />
+    </div>
   );
+};
+
+Error.propTypes = {
+  status: PropTypes.string,
+  message: PropTypes.string,
 };
 
 export default Error;
