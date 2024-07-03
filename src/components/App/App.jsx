4b6857ac2395f732f88f88b8d52918b926840dc7
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-07-03 12:19:35 +0530

    add: token state to trigger re renders

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,15 @@
+import { useState } from 'react';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 import Main from '../Main/Main';
 
 const App = () => {
+  const [token, setToken] = useState(localStorage.getItem('token'));
+
   return (
     <>
-      <Header />
-      <Main />
+      <Header token={token} setToken={setToken} />
+      <Main token={token} setToken={setToken} />
       <Footer />
     </>
   );
