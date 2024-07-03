9aa1023c671d25c2d9eebfe26423b894b944b93b
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-07-03 12:26:22 +0530

    update: logout updates state

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,39 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-const Header = () => {
+const Header = ({ token, setToken }) => {
   return (
     <>
       <h1>Blog</h1>
       <nav>
         <ul>
-          <li>Home</li>
           <li>
-            <Link to='/login'>LogIn/SignUp</Link>
+            <Link to='/home'>Home</Link>
+          </li>
+          <li>
+            {token ? (
+              <Link
+                to='/login'
+                onClick={() => {
+                  localStorage.removeItem('token');
+                  setToken(null);
+                }}
+              >
+                Logout
+              </Link>
+            ) : (
+              <Link to='/login'>LogIn/SignUp</Link>
+            )}
           </li>
         </ul>
       </nav>
     </>
   );
+};
+
+Header.propTypes = {
+  token: PropTypes.string,
+  setToken: PropTypes.func,
 };
 
 export default Header;
