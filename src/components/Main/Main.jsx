b813b43a8f954b83a095a8934e598fff6756d2d1
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-07-03 12:21:52 +0530

    update: to handle condtional redering and state manipulation

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,13 +1,32 @@
-import { Outlet } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
+import Login from '../Form/Login';
+import Register from '../Form/Register';
+import Home from '../Home/Home';
+import Error from '../Error/Error';
+import PropTypes from 'prop-types';
 
-const Main = () => {
+const Main = ({ token, setToken }) => {
+  const { path } = useParams();
   return (
     <>
       <main>
-        <div>main condinatinal render home or login/signup</div> <Outlet />
+        {path === 'login' ? (
+          <Login setToken={setToken} token={token} />
+        ) : path === 'register' ? (
+          <Register setToken={setToken} token={token} />
+        ) : path === 'home' || path === undefined ? (
+          <Home token={token} />
+        ) : (
+          <Error />
+        )}
       </main>
     </>
   );
+};
+
+Main.propTypes = {
+  token: PropTypes.string,
+  setToken: PropTypes.func,
 };
 
 export default Main;
