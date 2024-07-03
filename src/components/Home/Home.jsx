cd99cab770253ed602606eacbe97437465d185dc
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-07-03 12:24:31 +0530

    update: redirects if login

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,18 @@
+import { Navigate } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import './Home.css';
 
-const Home = () => {
+const Home = ({ token }) => {
+  if (!token) return <Navigate to={'/login'} />;
   return (
     <>
       <div>get all posts</div>
     </>
   );
+};
+
+Home.propTypes = {
+  token: PropTypes.string,
 };
 
 export default Home;
