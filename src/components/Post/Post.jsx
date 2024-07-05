8d93c4b0e3c775f3e26a0ce8be649c006f33729a
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-07-05 21:42:45 +0530

    add: Post component card

---
+++ src/components/Post/Post.jsx
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types';
+import distance from '../../utils/time_distance';
+import { Link } from 'react-router-dom';
+import './Post.css';
+
+const Post = ({ post }) => {
+  const createdTime = new Date(post.createdAt);
+  return (
+    <Link className='post' to={post._id}>
+      <div>
+        <div className='title'>{post.title}</div>
+        <div className='body'>{post.body}</div>
+        <div className='time'>Posted: {distance(createdTime)}</div>
+      </div>
+    </Link>
+  );
+};
+
+Post.propTypes = {
+  post: PropTypes.shape({
+    _id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    body: PropTypes.string.isRequired,
+    public: PropTypes.bool,
+    createdAt: PropTypes.string.isRequired,
+    updatedAt: PropTypes.string.isRequired,
+  }),
+};
+
+export default Post;
