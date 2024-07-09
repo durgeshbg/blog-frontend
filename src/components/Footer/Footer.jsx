62396f1536920bff78506bd9330c6ff4ab8fa097
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-07-10 01:33:40 +0530

    add: Footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,14 @@
+import { Link } from 'react-router-dom';
+
 const Footer = () => {
   return (
     <>
-      <footer>Footer</footer>
+      <footer>
+        Copyright &copy; 2024{' '}
+        <Link to='https://github.com/durgeshbg/blog-frontend' target='_blank'>
+          durgeshbg
+        </Link>{' '}
+      </footer>
     </>
   );
 };
